b751b0f4046f85be63d4ac6912a1baa94a753bee
MD~ <105758494+smTanmay@users.noreply.github.com>
2022-06-06 01:50:43 -0400

    I have done my selenium_cucumber_jave BDD Framework Test

---
+++ src/test/java/StepDefinitions/TestRunner.java
@@ -0,0 +1,18 @@
+package StepDefinitions;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/resources/Features",glue={"StepDefinitions"},
+monochrome=true,
+plugin= {"pretty","junit:target/JUniteports/report.xml",
+		
+		},
+
+tags="@smoketest"
+		)
+public class TestRunner {
+
+}
